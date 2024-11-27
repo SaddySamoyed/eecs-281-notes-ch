1d6285a3dd1479d566650136a70166e4dbb8905d
Qiulin <rynnefan@umich.edu>
2024-11-27 16:13:35 -0500

    i j

---
+++ Labs/Lab-10/writtem-knapsack/knapsack.h
@@ -0,0 +1,16 @@
+/*
+ * knapsack.h
+ * University of Michigan, Ann Arbor
+ * EECS 281 Lab 10 Written.
+ * DO NOT SUBMIT TO GRADESCOPE.
+ * If submitted, this file will be ignored.
+ */
+
+#ifndef KNAPSACK_H
+#define KNAPSACK_H
+
+#include <vector>
+
+int knapsack(int C, const std::vector<int>& value, const std::vector<int>& weight);
+
+#endif
