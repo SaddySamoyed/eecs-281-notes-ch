dca3679fb5d63a84fc221a73d60714d01cac0120
Qiulin <rynnefan@umich.edu>
2024-11-28 15:34:15 -0500

    ok

--- practice-code/commands.sh
+++ practice-code/commands.sh
@@ -1,3 +1,5 @@
+g++ -Wall -Werror -pedantic -g --std=c++17 -Wno-sign-compare -Wno-comment lec09-union-find.cpp -o lec09-union-find.exe
+./lec09-union-find.exe > lec09-union-find.out
 g++ -Wall -Werror -pedantic -g --std=c++17 -Wno-sign-compare -Wno-comment lec19-graph.cpp -o lec19-graph.exe
 ./lec19-graph.exe
 g++ -Wall -Werror -pedantic -g --std=c++17 -Wno-sign-compare -Wno-comment lec19-Dijkstra.cpp -o lec19-Dijkstra.exe
